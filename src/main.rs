ab699c36fd98d6ce6207a163b0fde48dc3557978
Otar Natsvaladze <71146188+Tagvi@users.noreply.github.com>
2023-04-05 15:52:42 +0400

    Added urgencies

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 extern crate notify_rust;
 extern crate tokio;
-use notify_rust::Notification;
+use notify_rust::{Notification, Urgency};
 use std::time::Duration;
 use tokio::{join, spawn, time};
 
 #[tokio::main]
 async fn main() {
-    let posture_notification =
-        RecurringNotification::new("Fix your posture", get_seconds_from_minutes(5.0));
-    let eyes_notification =
-        RecurringNotification::new("Time to let your eyes rest", get_seconds_from_minutes(30.0));
-    let (r1, r2) = join!(
+    let posture_notification = RecurringNotification::new(
+        "Fix your posture",
+        get_seconds_from_minutes(5.0),
+        Urgency::Normal,
+    );
+    let eyes_notification = RecurringNotification::new(
+        "Time to let your eyes rest",
+        get_seconds_from_minutes(30.0),
+        Urgency::Normal,
+    );
+    let eyedrops_notification = RecurringNotification::new(
+        "Time to use eyedrops",
+        get_seconds_from_minutes(60.0),
+        Urgency::Critical,
+    );
+    let (r1, r2, r3) = join!(
         spawn(posture_notification.start()),
-        spawn(eyes_notification.start())
+        spawn(eyes_notification.start()),
+        spawn(eyedrops_notification.start())
     );
     r1.unwrap();
     r2.unwrap();
+    r3.unwrap();
 }
 
 struct RecurringNotification {
@@ -24,9 +37,9 @@ struct RecurringNotification {
 }
 
 impl RecurringNotification {
-    fn new(summary: &str, seconds: u64) -> RecurringNotification {
+    fn new(summary: &str, seconds: u64, urgency: Urgency) -> RecurringNotification {
         let mut notification = Notification::new();
-        notification.summary(summary);
+        notification.summary(summary).urgency(urgency);
         RecurringNotification {
             time: Duration::from_secs(seconds),
             notification,
